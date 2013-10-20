38aeb6141050d368332d4c7bbb49a65a8d330ffc
vinoth06 <vinoth06@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-20 04:34:44 +0000

    First release 1.0 uploaded
    git-svn-id: https://plugins.svn.wordpress.org/a-very-simple-captcha/tags/1.0@790749 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ a-very-simple-captcha.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: A Very Simple Captcha
+Plugin URI: http://buffercode.com/
+Description: This Plugin provides high level of captcha for your blog with wide variety of captcha methods and models.
+Version: 1.0
+Author: BufferCode
+Author URI: http://buffercode.com/
+License: GPLv2
+*/
+
+//Menu added//
+include('include/bc_very-simple-captcha-menu.php');
+//Sorting concepts added//
+include('include/bc_very-simple-captcha-sort.php');
+
+?>
