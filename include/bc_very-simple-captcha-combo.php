9ff9f3ae166ac06e5d62c194fedddb775a97b5fa
vinoth06 <vinoth06@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-25 14:23:55 +0000

    1.5 added
    git-svn-id: https://plugins.svn.wordpress.org/a-very-simple-captcha/tags/1.5@793641 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ include/bc_very-simple-captcha-combo.php
@@ -0,0 +1,45 @@
+<?php
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+global $wp_version;
+if ( version_compare( $wp_version,'3','>=' ) ) {
+		add_action( 'comment_form_after_fields', 'bc_combo_comment_after', 1 );
+		add_action( 'comment_form_logged_in_after', 'bc_combo_comment_after', 1 );
+	}
+add_filter( 'preprocess_comment', 'bc_combo_comment_post' );	
+
+//Function for setting Combo box
+function bc_combo_comment_after(){?>
+<select name="bc_combo_captcha">
+<option value="1" >Am</option>
+<option value="2">Am Not</option>
+</select>
+<label>Spammer!</label>
+<?php
+}	
+
+function bc_combo_comment_post($comment){
+// skip captcha for comment replies from the admin menu
+		if ( isset( $_REQUEST['action'] ) && $_REQUEST['action'] == 'replyto-comment' ) {
+			// skip capthca
+			return $comment;
+		}
+
+// Skip captcha for trackback or pingback	
+if ( $comment['comment_type'] != '' && $comment['comment_type'] != 'comment' ) {
+							 // skip captcha
+							 return $comment;
+		}
+
+// Checking for Captcha matching			
+if ($_REQUEST['bc_combo_captcha']==2) {
+		return($comment);
+}
+else {
+		//die(print_r($_REQUEST['bc_combo_captcha']));
+		wp_die( __('Error: Incorrect CAPTCHA, Please click back and reenter', 'bc_combo_simple_captcha'));
+}
+}
+
+?>
---
+++ include/bc_very-simple-captcha-menu.php
@@ -0,0 +1,79 @@
+<?php
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+//adding very simple captcha menu
+add_action('admin_menu', 'bc_simple_captcha_menu');
+
+function bc_simple_captcha_menu() {
+
+	add_menu_page('simple captcha id', 'Simple Captcha', 'administrator', __FILE__, 'bc_simple_captcha_settings');
+
+	//call register settings function
+	add_action( 'admin_init', 'bc_simple_captcha_register_settings' );
+}
+
+
+function bc_simple_captcha_register_settings() {
+
+register_setting( 'bc-simple-captcha-settings-group', 'bc_sort_location' );
+register_setting( 'bc-simple-captcha-settings-group', 'bc_sort_key' );	
+}
+
+function bc_simple_captcha_settings() {
+?>
+<div class="wrap">
+<h2>Very Simple Captcha Settings</h2>
+<?php
+$bc_sort_location_value=get_option('bc_sort_location');
+$bc_sort_key_value=get_option('bc_sort_key');
+?>
+
+<!-- The below line will used to show the options were saved -->
+<?php if( isset($_GET['settings-updated']) ) { ?>
+    <div id="message" class="updated fade">
+        <p><strong><?php _e('Options Saved...') ?></strong></p>
+    </div>
+<?php } ?>
+
+<form method="post" action="options.php">
+    <?php settings_fields( 'bc-simple-captcha-settings-group' ); ?>
+    <?php do_settings_sections( 'bc-simple-captcha-settings-group' );?>
+	 
+    <table class="form-table">
+        <tr valign="top">
+        <th scope="row"><?php _e("<b>Select the captcha method</b>",'buffercode_captcha'); ?></th>
+        <td>
+		<!-- Adding Radio Button to select  -->
+		<input type="radio" name="bc_sort_location" value="1"<?php checked( '1', $bc_sort_location_value); ?>> <?php _e("Numbers Sort in Increasing Order",'buffercode_captcha'); ?> <br><br>
+		
+		<input type="radio" name="bc_sort_location" value="2"<?php checked( '2', $bc_sort_location_value); ?>>  <?php _e("Numbers Sort in Decreasing Order",'buffercode_captcha'); ?> <br><br>
+		
+		<input type="radio" name="bc_sort_location" value="3"<?php checked( '3', $bc_sort_location_value); ?>>  <?php _e("Combo Box Option",'buffercode_captcha'); ?> <br><br>
+		
+		<input type="radio" name="bc_sort_location" value="4"<?php checked( '4', $bc_sort_location_value); ?>>  <?php _e("Biggest or Smallest Number",'buffercode_captcha'); ?> <br><br>
+		
+		<input type="radio" name="bc_sort_location" value="5"<?php checked( '5', $bc_sort_location_value); ?>>  <?php _e("By Alphabets",'buffercode_captcha'); ?> <br><br>
+		
+			<input type="radio" name="bc_sort_location" value="6"<?php checked( '6', $bc_sort_location_value); ?>>  <?php _e("Addition or Subraction",'buffercode_captcha'); ?> <br><br>
+		</td>
+        </tr>
+		
+		<tr valign="top">
+        <th scope="row"><?php _e("<b>Key</b>",'buffercode_captcha'); ?></th>
+        <td>
+		<input type="text" maxlength="15" name="bc_sort_key" value="<?php echo $bc_sort_key_value; ?>"> <?php _e("<b>[Please use random text and symbols for more security]</b>",'buffercode_captcha'); ?> <br><br>
+		
+		</td>
+        </tr>
+		<tr valign="top">
+        <th scope="row"><?php _e("Designed by -",'buffercode_captcha'); ?> <a href="http://buffercode.com">Buffercode</a></th>
+        </tr>
+    </table>
+
+    <?php submit_button(); ?>
+
+</form>
+</div>
+<?php } ?>
