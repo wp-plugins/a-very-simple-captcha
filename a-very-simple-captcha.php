2f371c5b62e8e60342fc13d4086d968814842085
vinoth06 <vinoth06@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-19 05:50:59 +0000

    Very Simple Captcha Update 2.4.2, Support 3.9.1
    git-svn-id: https://plugins.svn.wordpress.org/a-very-simple-captcha/tags/2.4.2@916939 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ a-very-simple-captcha.php
@@ -0,0 +1,54 @@
+<?php
+/*
+Plugin Name: A Very Simple Captcha
+Plugin URI: http://buffercode.com/simple-captcha-for-wordpress/
+Description: This Plugin provides high level of captcha for your blog with wide variety of captcha methods and models.
+Version: 2.4.2
+Author: vinoth06
+Author URI: http://buffercode.com/
+License: GPLv2
+*/
+
+//Menu added//
+include('include/bc_very-simple-captcha-menu.php');
+
+function bc_very_simple_language() {
+  load_plugin_textdomain( 'bc-language', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+}
+add_action('plugins_loaded', 'bc_very_simple_language');
+
+//Adding CSS Font
+function bc_very_simple_captcha_font_css() {
+wp_enqueue_style( 'captcha-style',plugins_url('css\captcha-style.css',__FILE__) );
+}
+add_action( 'wp_enqueue_scripts', 'bc_very_simple_captcha_font_css' );
+//Sorting concepts added//
+
+$bc_sort_location_value=get_option('bc_sort_location');
+//Value selected from Admin dashboard
+
+if($bc_sort_location_value==1){
+include('include/bc_very-simple-captcha-sort.php');
+}
+elseif($bc_sort_location_value==2)
+{
+include('include/bc_very-simple-captcha-combo.php');
+}
+elseif($bc_sort_location_value==3)
+{
+include('include/bc_very-simple-captcha-big-small.php');
+}
+elseif($bc_sort_location_value==4)
+{
+include('include/bc_very-simple-captcha-alphabets.php');
+}
+elseif($bc_sort_location_value==5)
+{
+include('include/bc_very-simple-captcha-add-sub.php');
+}
+elseif($bc_sort_location_value==6)
+{
+include('include/bc_very-simple-captcha-checkbox.php');
+}
+
+?>
